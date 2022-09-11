e60fa03865712e6f4625a418b3b3fc8786a7f834
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-11 18:46:18 +0530

    fibonacci Problem

--- src/com/bridgelabz/logicprogram/FibonacciSeries.java
+++ src/com/bridgelabz/logicprogram/FibonacciSeries.java
@@ -1,5 +1,17 @@
 package com.bridgelabz.logicprogram;
 
 public class FibonacciSeries {
+	 public static void main(String[] args) {
+	        int num1=0;
+	        int num2=1;
 
+	        System.out.println(num1+"\n"+num2);
+
+	        for (int i = 0; i < 10; i++) {
+	            int num3=num1+num2;
+	            System.out.println(num3);
+	            num1=num2;
+	            num2=num3;
+	        }
+	    }
 }
